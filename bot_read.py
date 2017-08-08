8544dbf100e93b008cdaafc7faa5a13669c4d476
Mijail Montero <mijailmonterob@gmail.com>
2017-08-08 14:33:24 -0400

    adding the bot_read process

---
+++ bot_read.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/python
+import praw
+
+reddit = praw.Reddit('bot1')
+
+subreddit = reddit.subreddit("pythonforengineers")
+
+for submission in subreddit.hot(limit=5):
+	print("Title: ", submission.title)
+	print("Text: ", submission.selftext)
+	print("Score: ", submission.score)
+	print("---------------------------------\n")
